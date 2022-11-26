3d75bd05b99b0a893f3b9a6380a875bde0751dda
Jayce Sagvold <97198910+jSagvold28@users.noreply.github.com>
2022-11-26 16:00:58 -0600

    IN PROGRESS

---
+++ PlaylistScreen.swift
@@ -0,0 +1,45 @@
+//
+//  PlaylistScreen.swift
+//  SpotiyNowPlaying
+//
+//  Created by Jayce Sagvold on 11/26/22.
+//
+
+import SwiftUI
+
+struct PlaylistScreen: View {
+    var body: some View {
+        VStack {
+            
+            Image("PlaylistImage")
+                .frame(width: 340, height: 275)
+                .padding(.top, 100)
+                .padding(.bottom, 100)
+            
+            //Spacer()
+            
+            // List of songs
+            
+            VStack {
+                HStack {
+                    Image("SongCover-1")
+                        .frame(width: 50, height: 50)
+                        .padding(.horizontal, 15)
+                    Text("Dreams")
+                        .font(.subheadline)
+                    
+
+                
+                }
+            }
+        }
+        
+    }
+}
+
+
+struct PlaylistScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        PlaylistScreen()
+    }
+}
